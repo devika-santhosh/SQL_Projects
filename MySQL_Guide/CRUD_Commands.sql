f098bc12e45cb809a645ed1fdef37ec73632240f
Devika Santhosh <devikasanthosh04@gmail.com>
2024-04-18 09:59:21 +0530

    Added CRUD challenges

--- MySQL_Guide/CRUD_Commands.sql
+++ MySQL_Guide/CRUD_Commands.sql
@@ -62,3 +62,70 @@ WHERE cat_id = age;
 
 #DELETE all cats
 DELETE FROM cats;
+    #--------------------------#
+    
+/*
+Shirt table
+*/ /*
+shirt_id	article			color	shirt_size	last_worn
+---------------------------------------------------------
+1			t-shirt			white	S			10
+2			t-shirt			green	S			200
+3			polo shirt		black	M			10
+4			tank top		blue	S			50
+5			t-shirt			pink	S			0
+6			polo shirt		red		M			5
+7			tank top		white	S			200
+8			tank top		blue	M			15
+
+*/
+/*create table shirts*/
+CREATE TABLE shirts(
+	shirt_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	article VARCHAR(50),
+	color VARCHAR(50),
+	shirt_size VARCHAR(1),
+	last_worn INT
+);
+
+SHOW TABLES;
+
+DESC shirts;
+
+
+/*add values*/
+INSERT INTO shirts(article,color,shirt_size,last_worn)
+VALUES ('t-shirt', 'white', 'S', 10),
+('t-shirt', 'green', 'S', 200),
+('polo shirt', 'black', 'M', 10),
+('tank top', 'blue', 'S', 50),
+('t-shirt', 'pink', 'S', 0),
+('polo shirt', 'red', 'M', 5),
+('tank top', 'white', 'S', 200),
+('tank top', 'blue', 'M', 15);
+
+/*Add A New Shirt
+Purple polo shirt, size M last worn 50 days ago*/
+INSERT INTO shirts(article,color,shirt_size,last_worn)
+VALUES ('polo shirt','purple','M',50);
+
+
+/*Update all polo shirts
+Change their size to L*/
+UPDATE shirts
+SET shirt_size = 'L'
+WHERE article = 'polo shirt';
+
+/*alter table of shirtsize*/
+ALTER TABLE shirts
+MODIFY COLUMN shirt_size VARCHAR(2);
+
+/*Update all white shirts
+Change size to 'XS' and color to 'off white'*/
+UPDATE shirts
+SET shirt_size = 'XS',
+	color = 'off white'
+WHERE color = 'white';
+
+/*Delete all shirts*/
+DELETE FROM shirts;
